524d6a111a684d60ab3a2772a1bbaf4754dd638b
jacoboalzate02 <jacoboalzate@gmail.com>
2026-02-17 18:57:10 -0500

    cesar te amo

--- 19-laravel/database/factories/UserFactory.php
+++ 19-laravel/database/factories/UserFactory.php
@@ -24,10 +24,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'document' => fake()->numerify('75######'),
+            'fullname' => fake()->name(),
+            'gender' => fake()->randomElement(['Male', 'Female']),
+            'birthdate' => fake()->date(),
+            'phone' => fake()->numerify('320#######'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('12345'),
             'remember_token' => Str::random(10),
         ];
     }
