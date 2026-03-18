eeb629d46ca5f2a85d154de92a5ab67e3256303b
jacoboalzate02 <jacoboalzate@gmail.com>
2026-03-17 21:12:31 -0500

    larapets

--- 19-laravel/resources/views/auth/register.blade.php
+++ 19-laravel/resources/views/auth/register.blade.php
@@ -1,52 +1,91 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts.app')
+@section('title', 'Larapets: Register')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+@include('partials.navbar')
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+@section('content')
+    <section class="bg-[#0009] outline w-80 md:w-fit flex flex-col justify-center text-white items-center p-4 rounded-sm">
+        <h1 class="text-4xl flex gap-2 border-b-2 pb-2 mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-10" width="32" height="32" fill="currentColor"
+                viewBox="0 0 256 256">
+                <path
+                    d="M256,136a8,8,0,0,1-8,8H232v16a8,8,0,0,1-16,0V144H200a8,8,0,0,1,0-16h16V112a8,8,0,0,1,16,0v16h16A8,8,0,0,1,256,136Zm-57.87,58.85a8,8,0,0,1-12.26,10.3C165.75,181.19,138.09,168,108,168s-57.75,13.19-77.87,37.15a8,8,0,0,1-12.25-10.3c14.94-17.78,33.52-30.41,54.17-37.17a68,68,0,1,1,71.9,0C164.6,164.44,183.18,177.07,198.13,194.85ZM108,152a52,52,0,1,0-52-52A52.06,52.06,0,0,0,108,152Z">
+                </path>
+            </svg>
+            Register
+        </h1>
+        <form class="flex flex-col md:flex-row gap-4 gap-x-4" method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="w-full md:w-80">
+                {{-- Document --}}
+                <label class="label text-white mt-4">Document:</label>
+                <input class="input bg-[#0009] outline-1 focus:border-white w-full" type="text" name="document"
+                    value="{{ old('document') }}" placeholder="75000010">
+                @error('document')
+                    <small class="badge badge-error w-full">{{ $message }}</small>
+                @enderror
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                {{-- Ful Name --}}
+                <label class="label text-white mt-4">Full Name:</label>
+                <input class="input bg-[#0009] outline-1 focus:border-white w-full" type="text" name="fullname"
+                    value="{{ old('fullname') }}" placeholder="Jeremias Springfield">
+                @error('fullname')
+                    <small class="badge badge-error w-full">{{ $message }}</small>
+                @enderror
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                {{-- Gender --}}
+                <label class="label text-white mt-4">Gender:</label>
+                <select class="select bg-[#0009] outline-1 focus:border-white w-full" name="gender">
+                    <option value="">Select...</option>
+                    <option value="Female" @if(old('gender')=='Female') selected @endif>Female</option>
+                    <option value="Male" @if(old('gender')=='Male') selected @endif>Male</option>
+                </select>
+                @error('gender')
+                    <small class="badge badge-error w-full">{{ $message }}</small>
+                @enderror
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                {{-- Birthdate --}}
+                <label class="label text-white mt-4">Birthdate:</label>
+                <input class="input bg-[#0009] outline-1 focus:border-white w-full" type="text" name="birthdate"
+                    value="{{ old('birthdate') }}" placeholder="1990-12-25">
+                @error('birthdate')
+                    <small class="badge badge-error w-full">{{ $message }}</small>
+                @enderror
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            </div>
+            <div class="w-full md:w-80">
+                {{-- Phone --}}
+                <label class="label text-white mt-4">Phone:</label>
+                <input class="input bg-[#0009] outline-1 focus:border-white w-full" type="text" name="phone"
+                    value="{{ old('phone') }}" placeholder="320000010">
+                @error('phone')
+                    <small class="badge badge-error w-full">{{ $message }}</small>
+                @enderror
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                {{-- email --}}
+                <label class="label text-white mt-4">E-mail:</label>
+                <input class="input bg-[#0009] outline-1 focus:border-white w-full" type="text" name="email"
+                    value="{{ old('email') }}" placeholder="jeremias@mail.com">
+                @error('email')
+                    <small class="badge badge-error w-full">{{ $message }}</small>
+                @enderror
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                {{-- Password --}}
+                <label class="label text-white mt-4">Password:</label>
+                <input class="input bg-[#0009] outline-1 focus:border-white w-full" type="password" name="password"
+                    placeholder="yourSecret_123">
+                @error('password')
+                    <small class="badge badge-error w-full">{{ $message }}</small>
+                @enderror
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+                {{-- Password Confirmation--}}
+                <label class="label text-white mt-4">Password Confirmation:</label>
+                <input class="input bg-[#0009] outline-1 focus:border-white w-full" type="password"
+                    name="password_confirmation" placeholder="your Secret again">
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                <button class="btn btn-outline mt-4 w-full">Register</button>
+            </div>
+
+        </form>
+    </section>
+@endsection
---
+++ 19-laravel/resources/views/users/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Larapets: Module Users')
+
+@section('content')
+
+@endsection
