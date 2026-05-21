6f72c0a193601d46358f10df06a07a13a5e05900
Cristian4201 <benavides4ca@gmail.com>
2026-05-21 18:57:20 -0500

    .

--- 20-larapi/routes/web.php
+++ 20-larapi/routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "<main style='min-height: 100dvh
-                         display";
+    return "<main style='min-height: 100dvh;
+                         display: flex; 
+                         justify-content: center;
+                         align-items:center'>
+                <h1 style='font-family: Arial; font-size: 2rem'>
+                ✅ Check API Endpoints in postman/ApiDog 🚀
+                </h1>
+    </main>";
 });
